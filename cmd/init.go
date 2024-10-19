b1c1971ea10c958ff89f29d5f63251f984ad4ef2
iton0 <iton442@gmail.com>
2024-10-19 11:23:21 -0400

    wip: finished majority of logic for init command

--- cmd/init.go
+++ cmd/init.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
+	"github.com/iton0/hookup/pkg/logic"
 	"github.com/spf13/cobra"
 )
 
 var (
 	initCmd = &cobra.Command{
+		// TODO check docs about argument restrictions and all dat
 		Use:   "init",
 		Short: "Initialize Hookup.",
 		Run: func(cmd *cobra.Command, args []string) {
-			// TODO implement logic
+			logic.Init(Path)
+
 		},
 	}
 
 	// flags
-	Worktree string
-	Path     string
+	Path string
 )
 
 func init() {
-	initCmd.Flags().StringVarP(&Worktree, "worktree", "wt", "", "The current working directory is a worktree")
 	initCmd.Flags().StringVarP(&Path, "path", "p", "", "Specify path to initialize hookup folder")
 	rootCmd.AddCommand(initCmd)
 }
--- pkg/logic/init.go
+++ pkg/logic/init.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os/exec"
 
-	"github.com/iton0/hookup/cmd"
 	"github.com/iton0/hookup/pkg/util"
 )
 
@@ -14,26 +13,19 @@ const defaultFolder = ".hookup"
 // Defaults to current working directory; how to change?
 var path = []string{"."}
 
-func Init() {
+// INFO: main logic function for command will have to take all flags as arguments
+func Init(userPath string) {
 	var fullPath string
 
-	// Check for the flag arguements
-
-	// TODO: how do i properly do the worktree flag because setup may differ.
-	// The portable way is the use relative path but i need to make sure that I properly use relative
-	if cmd.Worktree != "" {
-		// Check if it is a worktree via does .git directory exist in cwd
-	}
+	// Check for the flag arguments
 	// User provided custom path; update path var
-	if cmd.Path != "" {
-		path = util.SplitFilePath(cmd.Path)
+	if userPath != "" {
+		path = util.SplitFilePath(userPath)
 	}
-
-	// Possible design choice: provide a convention in the README for where to place the defaultFolder. Hookup will only look in those
 
 	// check if there is a hookup folder and create if necessary
 	if !util.DoesDirectoryExist(path) {
-		var err error // is there a better way to do this?
+		var err error
 		fullPath, err = util.CreateFolder(path, defaultFolder)
 		fmt.Println("Error:", err)
 	}
@@ -43,7 +35,7 @@ func Init() {
 
 	_, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Error updating hooksPath:", err)
 		return
 	}
 }
